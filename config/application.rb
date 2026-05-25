b02224aa477681164c7124f94f231c7b2824d756
Bob Wang <7777@hey.com>
2026-05-25 09:53:26 -0400

    Add locale-aware translations

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module Smarts
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = [ :en, :"zh-CN", :"zh-TW" ]
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
   end
 end
